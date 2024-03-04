5988178f8d8078df2f2ccea279fc0c74ed7a2f94
Marcario <christinenalubwama4@gmail.com>
2024-03-04 10:37:53 +0300

    intial commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import Provider from "@/context/Providers";
 import Providers from "@/context/Providers";
 
